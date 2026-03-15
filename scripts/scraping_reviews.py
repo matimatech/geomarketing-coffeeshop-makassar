48294e069265af4ffd525f984f7ce3c8373542a0
matimatech <matimatechz@gmail.com>
2026-03-15 22:19:21 +0800

    add scraper for reviews, analyze the reviews

--- scripts/scraping_reviews.py
+++ scripts/scraping_reviews.py
@@ -1,12 +1,9 @@
 import time
-import re
 import pandas as pd
 
 from playwright.sync_api import sync_playwright
-# from playwright_stealth import stealth_sync
 
-
-PROVINCES = ["Aceh", "Bali", "Banten"]
+RESTOS = ["Onkel John's Coffee Hasanuddin", "Goodfields", "MyCoffee"]
 
 max_scrolls = 10
 scroll_pause = 2
@@ -14,59 +11,73 @@ scroll_pause = 2
 with sync_playwright() as p:
     data = []
 
-
-    for city in PROVINCES:
+    for resto in RESTOS:
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
         page.goto("https://www.google.com/maps")
-        # time.sleep(5)
         page.wait_for_timeout(1000)
-        # for city in PROVINCES:
-        query = f"Mie Gacoan {city}"
 
+        query = f"{resto}"
         search = page.locator('#ucc-1')
-
         search.fill(query)
         page.wait_for_timeout(1000)
 
-        # search.press("Enter")
         page.keyboard.press("Enter")
         page.wait_for_timeout(1000)
 
-        page.wait_for_selector('div[role="feed"]', timeout=50000)
+        if page.locator("div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde.ecceSd").count() > 0:
+            page.locator("a.hfpxzc").first.click()
+ 
+        page.get_by_role("tab", name=f"Ulasan untuk {resto}").click()
 
+        scrollabel_selector = 'div[role="main"]'
+        page.wait_for_selector(scrollabel_selector, timeout=50000)
+
+        last_height = 0
+        review_data = []
+        before = 0
+        after = 0
         while True:
-            # Paksa scroll elemen daftar ke paling bawah
+            before = len(review_data)
+            more_button = page.query_selector_all('button:has-text("Lainnya")')
+            for btn in more_button:
+                if btn.is_visible():
+                    btn.click()
+
             page.evaluate("""
-                const feed = document.querySelector('div[role="feed"]');
-                if (feed) { feed.scrollTo(0, feed.scrollHeight); }
+                const feed = document.querySelector('div.m6QErb.DxyBCb.kA9KIf.dS8AEf.XiKgde');
+                if (feed) {
+                          feed.scrollTo(0, feed.scrollHeight);
+                    }
             """)
-            time.sleep(2) # Jeda agar tidak terdeteksi sebagai bot
-                # Cek apakah sudah sampai akhir daftar
-            end_text_visible = page.evaluate("""
-                () => {
-                    const xpath = "//*[contains(text(), 'Anda telah mencapai akhir daftar') or contains(text(), 'reached the end of the list')]";
-                    const textNodes = document.evaluate(xpath, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-                    return textNodes.snapshotLength > 0;
-                }
-            """)
+            time.sleep(2) 
+
+            review_elements = page.locator('.jftiEf.fontBodyMedium').all()
             
-            if end_text_visible:
+            for review in review_elements:
+                text_element = review.locator('span.wiI7pd')
+                if text_element.count() > 0:
+                    text_element = text_element.inner_text()
+                else:
+                    text_element = ""
+
+                name = review.locator('.d4r55').inner_text()
+                rating = review.locator(".kvMYJc").get_attribute('aria-label')
+
+                entry = {"nama": name, "reviews": text_element, "rating": rating}
+                if entry not in review_data:
+                    review_data.append(entry)
+                    after = len(review_data)
+                    print(len(review_data))
+
+            if before == after:
                 break
-            links = page.query_selector_all('a[href*="maps/place"]')
-            for link in links:
-                name = link.get_attribute("aria-label")
-                href = link.get_attribute("href")
-                if name and href:
-                    data.append({
-                        "name": name,
-                        "url": href,
-                        # "province": province
-            })
-    df = pd.DataFrame(data)
-    df.to_csv("../data/data_gacoan.csv", index=False)
-    print(f"Saved {len(df)} records to maps_data_playwright.csv")
+
+        df = pd.DataFrame(review_data)
+        print(df)
+        print(f"Saved {len(df)} records to reviews_{resto}.csv")
+        df.to_csv(f"dataset/reviews_{resto}.csv", index=False)
 
     browser.close()
 
